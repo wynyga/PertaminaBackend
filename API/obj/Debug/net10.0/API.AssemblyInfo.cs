8d84a474e7204cd4b2abeed72d3cbdab3b63e82c
Can <wayanyoga32@gmail.com>
2026-08-31 09:34:40 +0700

    Fix EF Core version mismatch with Pomelo MySQL

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8596d27cc02576f46da98d4a06c8fc9c5c22fb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3389ac87f97fd1c93895abc6c4c4c4a478ed1489")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8596d27cc02576f46da98d4a06c8fc9c5c22fb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3389ac87f97fd1c93895abc6c4c4c4a478ed1489")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
